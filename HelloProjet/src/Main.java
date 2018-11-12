36a9402b00b0bc0e64a273e6a6399d463c02d239
lipengcheng <lpc_workemail@163.com>
2018-11-12 19:47:15 +0800

    修改代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("啦啦啦啦 ");
+        System.out.println("192.163.000");
     }
 }
